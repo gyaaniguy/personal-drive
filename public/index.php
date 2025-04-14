5b7b3e3c540bd554a525102ad78216cd51f6f287
Nikhil Jain <nik.jain@gmail.com>
2025-04-14 14:52:55 +0530

    tweak

--- public/index.php
+++ public/index.php
@@ -14,13 +14,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-try {
-    (require_once __DIR__.'/../bootstrap/app.php')->handleRequest(Request::capture());
-} catch (Throwable $e) {
-    if (str_contains($e->getMessage(), 'readonly database') || str_contains($e->getMessage(), 'open database')) {
-        http_response_code(500);
-        echo 'Database error: check permissions on database.sqlite';
-        exit;
-    }
-}
+
+(require_once __DIR__.'/../bootstrap/app.php')->handleRequest(Request::capture());
