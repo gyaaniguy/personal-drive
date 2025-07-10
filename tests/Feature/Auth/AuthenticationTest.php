54f964530b22f1bc6ba1ea1dc58fa3215bfb6652
Nikhil Jain <nik.jain@gmail.com>
2025-07-10 15:07:38 +0530

    repeat test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -9,11 +9,7 @@ class AuthenticationTest extends TestCase
 
     public function test_login_screen_can_be_rendered()
     {
-        auth()->logout();
-        $this->flushSession();
-
         $response = $this->get('/login');
-
         $response->assertStatus(302);
     }
 }
