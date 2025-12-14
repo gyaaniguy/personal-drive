29be7667b81dd2b362b8704cac6553fdc8c24efb
Nikhil Jain <nik.jain@gmail.com>
2025-12-14 22:57:07 +0530

    mobile layout improvements

--- resources/js/Pages/Drive/Components/FileBrowserSection.jsx
+++ resources/js/Pages/Drive/Components/FileBrowserSection.jsx
@@ -189,7 +189,7 @@ const FileBrowserSection = memo(({ files, path, token, isAdmin, slug }) => {
     }, [selectedFiles]);
 
     return (
-        <div className="min-h-screen rounded-md px-0 md:px-4 ">
+        <div className="min-h-screen rounded-md px-3 md:px-4 ">
             <ShareModal
                 isShareModalOpen={isShareModalOpen}
                 setIsShareModalOpen={setIsShareModalOpen}
