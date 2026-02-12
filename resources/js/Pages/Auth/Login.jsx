af029374b22389bfda7f7f584c04ba8fff8a8796
Nikhil Jain <nik.jain@gmail.com>
2026-02-12 13:20:16 +0000

    remove remember me

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -67,20 +67,20 @@ export default function Login({ status }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData("remember", e.target.checked)
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-400">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
+                {/*<div className="mt-4 block">*/}
+                {/*    <label className="flex items-center">*/}
+                {/*        <Checkbox*/}
+                {/*            name="remember"*/}
+                {/*            checked={data.remember}*/}
+                {/*            onChange={(e) =>*/}
+                {/*                setData("remember", e.target.checked)*/}
+                {/*            }*/}
+                {/*        />*/}
+                {/*        <span className="ms-2 text-sm text-gray-400">*/}
+                {/*            Remember me*/}
+                {/*        </span>*/}
+                {/*    </label>*/}
+                {/*</div>*/}
 
                 <div className="mt-4 flex items-center justify-end">
                     <PrimaryButton className="ms-4" disabled={processing}>
