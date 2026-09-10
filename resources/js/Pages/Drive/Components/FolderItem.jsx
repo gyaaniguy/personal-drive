7ba305e6fe1ad10961c49a891e894c96fc352e88
Nikhil Jain <nik.jain@gmail.com>
2026-09-10 22:03:18 +0000

    Fix blank-page crash when searching from a folder with subfolders
    FolderItem did path.split() unguarded in its non-search branch, but
    /search-files passes no path prop. During the transition to results the
    previous folder's subfolder rows re-render before isSearch flips, hitting
    split() on undefined -> uncaught TypeError -> the whole SPA unmounts.
    Guard with (path ?? '').

--- resources/js/Pages/Drive/Components/FolderItem.jsx
+++ resources/js/Pages/Drive/Components/FolderItem.jsx
@@ -37,7 +37,7 @@ const FolderItem = React.memo(function FolderItem({
                                     .map(encodeURIComponent)
                                     .join("/") + "/"
                               : "")
-                        : path.split("/").map(encodeURIComponent).join("/") +
+                        : (path ?? "").split("/").map(encodeURIComponent).join("/") +
                           "/") + encodeURIComponent(file.filename)
                 }
                 className={`min-w-0 flex-1 ${isSelected ? "bg-blue-100" : ""}`}
